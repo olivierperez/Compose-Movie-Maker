bb0340e53fe84e685a00f9d9f8b77f3544ed5640
Olivier Perez <olivier.perez@zenika.com>
2023-09-13 20:21:46 +0200

    Generate a GIF example

--- script/generate_mp4.sh
+++ script/generate_mp4.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-FFMPEG="./ffmpeg.exe"
+FFMPEG="ffmpeg"
 CAPTURES_DIR="../captures"
-FPS=10
+FPS=30
+GIF_FPS=5
 
-[ -f $FFMPEG ] || { >&2 echo "You need to download ffmpeg here!" ; exit 1; }
+# [ -f $FFMPEG ] || { >&2 echo "You need to download ffmpeg here!" ; exit 1; }
 
 $FFMPEG -framerate "$FPS" -i "$CAPTURES_DIR/screenshot-%d.png" -c:v libx264 "$CAPTURES_DIR/out.mp4"
+$FFMPEG -framerate "$FPS" -i "$CAPTURES_DIR/screenshot-%d.png" -vf "fps=$GIF_FPS,format=gray" "$CAPTURES_DIR/out.gif"
